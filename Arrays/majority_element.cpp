54bd08bf2e2ef72087626ebacc3c36256e1562a7
Ansh Porwal <anshporwal2431218@gmail.com>
2026-05-14 14:41:22 +0530

    Solve Majority Element problem

---
+++ Arrays/majority_element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) 
+    {
+       int n = nums.size();
+       unordered_map<int,int>mp;
+
+       for(int i=0;i<n;i++)
+       {
+          mp[nums[i]]++;
+       }
+
+       for(int j=0;j<n;j++)
+       {
+          if(mp[nums[j]]>(n/2))
+            return nums[j];
+       }
+
+       return -1;
+    }
+};
